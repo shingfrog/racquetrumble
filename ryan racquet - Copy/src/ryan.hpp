c675d016ba546b1b090301131c8eead82c5ead13
shingfrog <0rb1t.teen@gmail.com>
2024-06-06 18:56:41 +0800

    Update ryan.hpp

--- ryan racquet - Copy/src/ryan.hpp
+++ ryan racquet - Copy/src/ryan.hpp
@@ -8,9 +8,20 @@ class Ryan
     ~Ryan();
     void Draw();
     void moveRyan();
+    float GetWidth();
+    float GetHeight();
+    void Reset();
+    Texture2D GetTexture();
 
     Vector2 position;
+    Texture2D texture;
+    
+    int health = 3;
     
     private:
-    Texture2D ryanImage;
+    Texture2D ryanImageUp = LoadTexture("media/faceUp.png");
+    Texture2D ryanImageLeft = LoadTexture("media/faceLeft.png");
+    Texture2D ryanImageRight = LoadTexture("media/faceRight.png");
+    Texture2D ryanImageDown = LoadTexture("media/faceDown.png");
+
 };
