c72e710972b3f2238f69d6cf79b4b5b0795af3eb
shingfrog <0rb1t.teen@gmail.com>
2024-06-06 18:53:39 +0800

    Update ball.hpp

--- ryan racquet - Copy/src/ball.hpp
+++ ryan racquet - Copy/src/ball.hpp
@@ -3,19 +3,21 @@
 #include "enemy.hpp"
 #include "ryan.hpp"
 
+class Game;
+
 class Ball
 {
     public:
     Ball(Vector2 position, Texture2D ballImage, double speed);
     ~Ball();
-    
     void Draw();
-    void Update();
+    void Update(Ryan* ryan);
 
     Texture2D GetTexture();
     Vector2 getBallPosition();
 
     bool active;
+    int score = 0;
 
     private:
     Texture2D ballImage;
