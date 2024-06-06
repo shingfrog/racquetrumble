21aa585ffcc325a085762d490804c952901a4e12
shingfrog <0rb1t.teen@gmail.com>
2024-06-06 18:56:03 +0800

    Update main.cpp

--- ryan racquet - Copy/src/main.cpp
+++ ryan racquet - Copy/src/main.cpp
@@ -3,14 +3,15 @@
 #include "button.hpp"
 
 int main() {
-    InitWindow(900, 900, "Racquet Rumble");
+    InitWindow(900, 900, "Ryan VS The Forces of Evil");
     SetTargetFPS(60); 
 
     int framesCounter = 0;
     GameScreen currentScreen = LOGO;
     Scenes scenes;
 
-    Button playButton = Button({ 350, 694, 200, 50 }, GREEN, BLACK, "Play"); 
+    Button playButton = Button({ 350, 694, 200, 50 }, GREEN, BLACK, "Play");
+    Button restartButton = Button({ 350, 500, 200, 50 }, RED, BLACK, "Restart");
 
     while (!WindowShouldClose()) {
         switch (currentScreen) {
@@ -21,10 +22,10 @@ int main() {
             scenes.updateTitleScreen(&currentScreen, playButton);
             break;
         case GAMEPLAY:
-            scenes.updateGameplayScreen(&currentScreen);
+            scenes.updateGameplayScreen(&currentScreen, restartButton);
             break;
         case ENDING:
-            scenes.updateEndingScreen(&currentScreen);
+            scenes.updateEndingScreen(&currentScreen, restartButton);
             break;
         default: 
             break;
