e2ebf728804ed72d52b92442d8d2d1b8ee00efe4
shingfrog <0rb1t.teen@gmail.com>
2024-06-06 18:52:54 +0800

    Update ball.cpp

--- ryan racquet - Copy/src/ball.cpp
+++ ryan racquet - Copy/src/ball.cpp
@@ -1,6 +1,4 @@
 #include "ball.hpp"
-#include <ctime>
-
 
 Ball::Ball(Vector2 position, Texture2D ballImage, double speed)
 {
@@ -12,7 +10,6 @@ Ball::Ball(Vector2 position, Texture2D ballImage, double speed)
 
 Ball::~Ball()
 {
-    //UnloadTexture(ballImage);
 }
 
 void Ball::Draw()
@@ -21,22 +18,32 @@ void Ball::Draw()
         DrawTextureV(ballImage, position, WHITE);
 }
 
-void Ball::Update()
+void Ball::Update(Ryan* ryan)
 {
     if (position.x == 450) {
-        if(position.y > 450)
+        if(position.y >= 450)
             position.y += -speed;
         else
             position.y += speed;
     } else if (position.y == 450) {
-        if(position.x > 450)
+        if(position.x >= 450)
             position.x += -speed;
         else
             position.x += speed;
     }
 
+    if (CheckCollisionRecs(Rectangle{position.x, position.y, 12, 12}, Rectangle{ryan->position.x, ryan->position.y, (float)ryan->GetWidth(), (float)ryan->GetHeight()})){
+        speed *= -1;
+    }
+
+    if (position.x >= 433 && position.x <= 450 && position.y >= 433 && position.y <= 450) {
+        active = false; 
+        ryan->health--;
+    }
+
+
     if(active){
-        if(position.x > GetScreenWidth() || position.y > GetScreenHeight() || position.x < 0 || position.x < 0 ){
+        if(position.x > GetScreenWidth() || position.y > GetScreenHeight() || position.x < 0 || position.y < 0 ){
             active = false;
         }
     }
@@ -51,49 +58,4 @@ Vector2 Ball::getBallPosition()
 {
     return position;
 }
-
-
-// int Ball::updateSpeed(){
-//     SetRandomSeed(time(NULL));
-//     return GetRandomValue(1, 2);
-// }
-
-// void Ball::updateX(Ryan* ryan)
-// {
-//     position.x += speed;
-
-//     int r = 6;
-//     if (position.x + r >= GetScreenHeight() || position.x - r <= 0) {
-//         speed *= -1;
-//     } else if (CheckCollisionRecs(Rectangle{position.x, position.y, 12, 12}, Rectangle{24, 442, 7, 20})) {
-//         speed *= -1;
-//         speed = updateSpeed();
-//     } else if (CheckCollisionRecs(Rectangle{position.x, position.y, 12, 12}, Rectangle{868, 438, 7, 20})) {
-//         speed = updateSpeed();
-//         speed *= -1;
-//     } else if (CheckCollisionRecs(Rectangle{position.x, position.y, 12, 12}, Rectangle{ryan->position.x, ryan->position.y, 20, 7})) {
-//         speed *= -1;
-//         score++;
-//     } 
-// }
-
-// void Ball::updateY(Ryan* ryan)
-// {
-//     position.y += speed;
-
-//     int r = 6;
-//     if (position.y + r >= GetScreenWidth() || position.y - r <= 0){
-//         speed *= -1;
-//     } else if (CheckCollisionRecs(Rectangle{position.x, position.y, 12, 12}, Rectangle{439, 29, 20, 7})) {
-//         speed *= -1;
-//         speed = updateSpeed();
-//     } else if (CheckCollisionRecs(Rectangle{position.x, position.y, 12, 12}, Rectangle{440, 862, 20, 7})) {
-//         speed = updateSpeed();
-//         speed *= -1;
-//     } else if (CheckCollisionRecs(Rectangle{position.x, position.y, 12, 12}, Rectangle{ryan->position.x, ryan->position.y, 20, 7})) {
-//         speed *= -1;
-//         score++;
-//     } 
-
-// }
 
